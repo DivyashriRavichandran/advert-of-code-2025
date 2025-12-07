a835ba237499a0e52fbfa235df9f09c1017dedcc
divyashri.ravichandran4 <divyashri.ravichandran4@gmail.com>
2025-12-07 17:12:08 +0300

    added part 2 of day 3

--- Day-03-Lobby/index.js
+++ Day-03-Lobby/index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-// Part 1: Calculate total output of battery banks
+// Part 1: Calculate total output of battery banks with 2-digit joltages
 const findTotalOutput = (banks) => {
   let totalOutput = 0;
 
@@ -30,7 +30,51 @@ const findTotalOutput = (banks) => {
   return totalOutput;
 };
 
+// Part 2: Calculate total output of battery banks with 12-digit joltages
+const findTotalOutputPart2 = (banks) => {
+  let totalOutput = 0;
+
+  // Get each bank of batteries
+  for (let bankIndex = 0; bankIndex < banks.length; bankIndex++) {
+    const bank = banks[bankIndex];
+    const digits = bank.split("").map(Number); // Convert bank string to array of digits
+
+    let toPick = 12; // we need to pick 12 digits
+    let maxJoltage = [];
+    let start = 0;
+
+    // loop to pick the highest possible digit for each position
+    while (toPick > 0) {
+      const end = digits.length - toPick; // calculate the end index for the current pick
+
+      // find the best digit in the current range
+      let bestDigit = -1;
+      let bestIndex = start;
+
+      // loop through the range to find the maximum digit
+      for (let i = start; i <= end; i++) {
+        if (digits[i] > bestDigit) {
+          bestDigit = digits[i];
+          bestIndex = i;
+        }
+      }
+
+      // add the best digit to the result and update the start index
+      maxJoltage.push(bestDigit);
+      start = bestIndex + 1;
+      toPick--;
+    }
+
+    // convert the array of digits back to a number
+    maxJoltage = maxJoltage.join("");
+    totalOutput += Number(maxJoltage);
+  }
+
+  return totalOutput;
+};
+
 // Read input from file
 const data = fs.readFileSync("input.txt", "utf-8");
 const banks = data.split("\n");
 console.log("Part 1: Total Output Joltage:", findTotalOutput(banks));
+console.log("Part 2: New Total Output Joltage:", findTotalOutputPart2(banks));
